f383ead3143f41f3da34dd960af2e4c5ca27141c
kumar-vivek-iitian <23je1091@iitism.ac.in>
2025-04-01 19:47:02 +0530

    directory structure

---
+++ backend/models/models.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	Username string `bson:"username"`
+	Userid   string `bson:"userid"`
+	Email    string `bson:"email"`
+	Password string `bson:"password"`
+}
+
+
---
+++ backend/routes/routes.go
@@ -0,0 +1,8 @@
+package routes
+
+import (
+	"github.com/Hackfest-Hectoc/HectoClash/backend/models"
+
+	"github.com/gorilla/mux"
+)
+
