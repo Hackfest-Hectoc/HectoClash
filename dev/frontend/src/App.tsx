90e76e9a5d8691c225cfbd7f907c9e8c57988435
Antriksh Gwal <antrikshgwal@gmail.com>
2025-04-03 01:17:27 +0530

    Add play area ui

--- dev/frontend/src/App.tsx
+++ dev/frontend/src/App.tsx
@@ -4,14 +4,16 @@ import SignUpPage from './components/Signup/SignUpPage';
 import SignIn from './components/Login/SignIn';
 import Home from './components/Home/Home';
 import Gameselect from './components/gameselect/gameselect';
+import MathGame from './components/gameselect/playArea';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignUpPage />} />
         <Route path='/signin' element={<SignIn />}/>
-        <Route path='/' element={<Home/>}/> 
-        <Route path='/gameselect' element={<Gameselect/>}/> 
+        <Route path='/' element={<Home/>}/>
+        <Route path='/gameselect' element={<Gameselect/>}/>
+        <Route path='/MathGame' element={<MathGame/>}/>
       </Routes>
     </Router>
   );
