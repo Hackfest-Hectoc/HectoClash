55dbb2e7ca703d0bd006a15fcd7a136877f75c48
kumar-vivek-iitian <23je1091@iitism.ac.in>
2025-04-04 17:39:31 +0530

    Updates

--- match-making-service/main.go
+++ match-making-service/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	_ "encoding/json"
 	"log"
+	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/Hackfest-Hectoc/HectoClash/match-making-service/models"
@@ -33,6 +37,7 @@ func MatchMakingService() {
 			continue
 		}
 		if count > 1 {
+			log.Println(count)
 			topTwo, err := rdb.ZPopMax(ctx, QUEUE_KEY, 2).Result()
 			if err == redis.Nil {
 				continue
@@ -48,22 +53,56 @@ func MatchMakingService() {
 			// If client does not use their gid within 60 seconds
 			// they will automatically be removed from the database
 			var gameState models.Game
+			var GameClient models.GameClient
 
 			gameState.ID = gid
 			gameState.Playerone = first
 			gameState.Playertwo = second
 			gameState.Status = "starting"
+			gameState.Player1CurrRound = 1
+			gameState.Player2CurrRound = 1
+			gameState.Player1LastSubmission = time.Now().Unix()
+			gameState.Player2LastSubmission = time.Now().Unix()
+			gameState.Questions = GenerateHectoc(5)
+			gameState.NoofRounds = 5
+			GameClient.ID = gid
+			GameClient.Playerone = first
+			GameClient.Playertwo = second
+			GameClient.Status = "starting"
+			GameClient.Player1CurrRound = 1
+			GameClient.Player2CurrRound = 1
+			GameClient.NoofRounds = 5
+			gamestatejson, _ := json.Marshal(gameState)
+			gameclientjson, _ := json.Marshal(GameClient)
+
+			rdb.Set(ctx, gid, string(gamestatejson), time.Minute*10)
+			rdb.Set(ctx, gid+"gameclient", string(gameclientjson), time.Minute*10)
 			rdb.Set(ctx, first, gid, time.Minute*10)
 			rdb.Set(ctx, second, gid, time.Minute*10)
-			rdb.Set(ctx, gid, gameState, time.Minute*10)
+
 			log.Println("GID ", gid, "SET")
 			log.Println("USERS: ", first, second, " RUNNING UNDER GID: ", gid)
 		}
 	}
 }
 
+func GenerateHectoc(count int) []string {
+	rand.Seed(time.Now().UnixNano()) // Seed to ensure randomness
+	var results []string
+	for j := 0; j < count; j++ {
+		var result string
+		for i := 0; i < 6; i++ {
+			digit := rand.Intn(9) + 1 // Random number between 1 and 9
+			result += strconv.Itoa(digit)
+		}
+		results = append(results, result)
+	}
+
+	return results
+}
+
 func main() {
 	Connect()
-	log.Println("CONNECTION SUCCESSFULL!")
+	log.Println("CONNECTION SUCCESSFULL")
 	MatchMakingService()
 }
--- match-making-service/models/models.go
+++ match-making-service/models/models.go
@@ -1,8 +1,38 @@
-package models 
+package models
+
+import "context"
+
+var ctx = context.TODO()
+
+// import "time"
 
 type Game struct {
-	ID string `json:"string"`
-	Playerone string `json:"player-one"`
-	Playertwo string `json:"player-two"`
-	Status string `json:"status"`
+	ID                    string   `json:"gid"`
+	Playerone             string   `json:"player-one"`
+	Playertwo             string   `json:"player-two"`
+	Status                string   `json:"status"`
+	Player1Expression     string   `json:"player1expression"`
+	Player2Expression     string   `json:"player2expression"`
+	Player1CurrRound      int64    `json:"player1curround"`
+	Player2CurrRound      int64    `json:"player2curround"`
+	Player1LastSubmission int64    `json:"player1lastsubmission"`
+	Player2LastSubmission int64    `json:"player2lastsubmission"`
+	Player1Points         int64    `json:"player1points"`
+	Player2Points         int64    `json:"player2points"`
+	Questions             []string `json:"questions"`
+	NoofRounds            int64    `json:"noofrounds"`
+}
+
+type GameClient struct {
+	ID                string `json:"string"`
+	Playerone         string `json:"player-one"`
+	Playertwo         string `json:"player-two"`
+	Status            string `json:"status"`
+	Player1Expression string `json:"player1expression"`
+	Player2Expression string `json:"player2expression"`
+	Player1CurrRound  int64  `json:"player1curround"`
+	Player2CurrRound  int64  `json:"player2curround"`
+	Player1Points     int64  `json:"player1points"`
+	Player2Points     int64  `json:"player2points"`
+	NoofRounds        int64  `json:"noofrounds"`
 }
